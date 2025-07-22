ae0a9ec69efb7ed1251f814ebee0031d7dbde8b6
Ali Hinnawe <hinnaweali@gmail.com>
2025-07-22 13:54:40 +0200

    load proj

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './templates/**/*.html.twig',
+    './assets/**/*.js',
+    './assets/**/*.css',
+    './src/**/*.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
