c05267435249a0e87b932768e1fbae91bc9c5f92
Ali Hinnawe <hinnaweali@gmail.com>
2025-07-22 17:37:26 +0200

    link the add new product button to the form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class ProductController extends AbstractController
 {
@@ -16,8 +20,25 @@ final class ProductController extends AbstractController
 
         return $this->render('admin/product/index.html.twig', [
             'controller_name' => 'ProductController',
-            'products' => $products, // pass products to the template
+            'products' => $products,
+        ]);
+    }
+
+    #[Route('admin/add/product', name: 'app_product_add')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product->setCreatedAt(new \DateTimeImmutable());
+
+            return $this->redirectToRoute('app_product');
+        }
+
+        return $this->render('admin/product/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
-
